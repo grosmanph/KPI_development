902e2e50b0973fac9bdf5d102e6ba4839602efa2
Pedro Grosman <pedroalves@id.uff.br>
2022-03-16 19:15:36 -0300

    Add files via upload

--- subroutines.py
+++ subroutines.py
@@ -8,6 +8,52 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
+def fft_denoiser(x, n_components, to_real=True):
+    """Fast fourier transform denoiser.
+    
+    Denoises data using the fast fourier transform.
+    
+    Inputs:
+    -------
+    x : numpy.array
+        The data to denoise.
+    n_components : float
+        The value above which the coefficients will be kept.
+    to_real : bool, optional, default: True
+        Whether to remove the complex part (True) or not (False)
+        
+   	Outputs:
+    -------
+    clean_data : numpy.array
+        The denoised data.
+        
+    References
+    ----------
+    .. [1] Steve Brunton - Denoising Data with FFT[Python]
+       https://www.youtube.com/watch?v=s2K1JfNR7Sc&ab_channel=SteveBrunton
+    
+    """
+    n = len(x)
+    
+    # compute the fft
+    fft = np.fft.fft(x, n)
+    
+    # compute power spectrum density
+    # squared magnitud of each fft coefficient
+    PSD = fft * np.conj(fft) / n
+    
+    # keep high frequencies
+    _mask = PSD > n_components
+    fft = _mask * fft
+    
+    # inverse fourier transform
+    clean_data = np.fft.ifft(fft)
+    
+    if to_real:
+        clean_data = clean_data.real
+    
+    return clean_data
+
 
 def reduce_mem_usage(df):
     """ 
